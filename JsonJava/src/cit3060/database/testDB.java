125519cb784af907a3232757f3d4b158a059f272
jsistemas <ricardoahuire@gmail.com>
2019-11-08 16:10:27 -0500

    Json Example

---
+++ JsonJava/src/cit3060/database/testDB.java
@@ -0,0 +1,14 @@
+package cit3060.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class testDB {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException{
+		// TODO Auto-generated method stub
+		Connection cn=AccessDB.getConnection();
+        System.out.println(cn+" Successful Connection ....");
+	}
+
+}
