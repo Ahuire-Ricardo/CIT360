0fe6b7d8aa74b14d82b29f166b3dc89411217e1b
jsistemas <ricardoahuire@gmail.com>
2019-11-07 20:31:24 -0500

    Ready!

--- HTTPUrlConecction/src/Conecction.java
+++ HTTPUrlConecction/src/Conecction.java
@@ -2,56 +2,62 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
 
+
 public class Conecction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// This is the url that the program will connect to.
 				String output = getUrlContents("https://ahuire-ricardo.github.io/food.json");
+				//String output = getUrlContents("https://apis.google.com/js/client.js?onload=onLoad");
 				
 				//This is the actual output. It will print out the html file for the url provided above.
 				System.out.println(output);
 
 	}
 	
-private static String getUrlContents(String theUrl) {
-		
-		// Try/catch statement to catch an exceptions being thrown. 
-		StringBuilder content = new StringBuilder();
-		
-		try {
+	private static String getUrlContents(String theUrl) {
 			
-			//URL object.
-			URL url = new URL(theUrl);
+			// Try/catch statement to catch an exceptions being thrown. 
+			StringBuilder content = new StringBuilder();
 			
-			//URLConnection object
-			URLConnection urlConnection = url.openConnection();
-			
-			//BufferedReader object.
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-			
-			String line;
-			
-			// Read from the urlconnection through bufferedreader.
-			while((line = bufferedReader.readLine()) != null) {
+			try {
 				
-				content.append(line + "\n");
+				//URL object.
+				URL url = new URL(theUrl);
+				
+				//URLConnection object
+				URLConnection urlConnection = url.openConnection();
+				
+				System.out.println("\nSending 'GET' request to URL : " + theUrl + "\n");
+								
+				//BufferedReader object.
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+				
+				String line;
+				
+								
+				// Read from the urlconnection through bufferedreader.
+				while((line = bufferedReader.readLine()) != null) {
+					
+					content.append(line + "\n");
+					
+						
+				}
+				
+				bufferedReader.close();
 				
 			}
 			
-			bufferedReader.close();
+			catch(Exception e)
 			
-		}
-		
-		catch(Exception e)
-		
-		{
+			{
+				
+				e.printStackTrace();
 			
-			e.printStackTrace();
-		
-		}
-		
-		return content.toString();
-}
+			}
+			
+			return content.toString();
+	}
 
 }
