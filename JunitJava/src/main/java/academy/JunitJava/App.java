be10c39cf6a1245d7785db9663bcaf358092ad74
jsistemas <ricardoahuire@gmail.com>
2019-11-08 12:13:48 -0500

    JUnit example.

---
+++ JunitJava/src/main/java/academy/JunitJava/App.java
@@ -0,0 +1,50 @@
+package academy.JunitJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private int ans;
+	
+	public App() {
+		ans = 0;
+	}
+    
+	public int sum(int a, int b) {
+		ans = a + b;
+		return ans;
+	}
+	
+	public int subtraction(int a, int b) {
+		ans = a - b;
+		return ans;
+	}
+	
+	public int sum(int v) {
+		ans += v;
+		return ans;
+	}
+	
+	public int subtraction(int v) {
+		ans -= v;
+		return ans;
+	}
+	
+	public int ans() {
+		return ans;
+	}
+	
+	public int div(int a, int b) {
+		if (b==0) {
+			throw new ArithmeticException("we can't divide a number by 0");
+		}
+		ans = a/b;
+		return ans;
+	}
+	
+	public void clear() {
+		ans = 0;
+	}
+}
---
+++ JunitJava/src/test/java/academy/JunitJava/AppTest.java
@@ -0,0 +1,109 @@
+package academy.JunitJava;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Unit test for simple App.
+ */
+
+@RunWith(value = Parameterized.class)
+public class AppTest {
+	
+	@Parameters
+	public static Iterable<Object[]> getData() {
+		return Arrays.asList(new Object[] [] {
+			{3,1,4}, {2,3,5}, {3,3,6}
+		});
+	}
+	
+	static App calc;
+	private int a, b, exp;
+	
+	public AppTest(int a, int b, int exp) {
+		this.a = a;
+		this.b = b;
+		this.exp = exp;
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("beforeClass()");
+		calc = new App();
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("afterClass()");
+		calc = new App();
+	}
+	
+	@Before
+	public void before() {
+		System.out.println("before()");
+		calc = new App();
+	}
+	
+	@After
+	public void after() {
+		System.out.println("after()");
+		calc.clear();
+	}
+	
+	@Test
+	public void testSum() {
+		System.out.println("sum()");
+		int res = calc.sum(a, b);
+		//int expected = 5;
+		assertEquals(exp,res);
+	}
+	
+	@Test
+	public void testAnsSum() {
+		System.out.println("ansSum()");
+		calc.sum(a, b);
+		int res = calc.ans();
+		//int expected = 5;
+		assertEquals(exp,res);
+	}
+    
+	@Test
+	public void testSubtraction() {
+		System.out.println("subtraction()");
+		int tot = calc.subtraction(3, 2);
+		int expected = 1;
+		assertEquals(expected,tot);
+	}
+	
+	@Test
+	public void testAnsSubtraction() {
+		System.out.println("ansSubtraction()");
+		calc.subtraction(3, 2);
+		int res = calc.ans();
+		int expected = 1;
+		assertEquals(expected,res);
+	}
+	
+	@Test
+	public void testDiv() {
+		System.out.println("Div()");
+		calc.div(a, b);
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDivZero() {
+		System.out.println("testDivZero()");
+		calc.div(a, 0);
+	}
+   
+}
